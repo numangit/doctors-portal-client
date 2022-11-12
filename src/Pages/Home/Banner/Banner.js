6f68f4b43a94927173f60ef2e87c8f2427082320
numangit <abdulnumanjob@gmail.com>
2022-11-12 21:17:43 +0600

    button component and appointment banner added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import chair from "../../../assets/images/chair.png";
+import PrimaryButton from '../../../components/PrimaryButton/PrimaryButton';
 const Banner = () => {
     return (
         <div>
@@ -9,8 +10,8 @@ const Banner = () => {
                     <img src={chair} className="rounded-lg lg:w-1/2 shadow-2xl" alt="" />
                     <div>
                         <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
-                        <p className="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
-                        <button className="btn border-0	text-white bg-gradient-to-r from-primary to-secondary">Get Started</button>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <PrimaryButton>Get Started</PrimaryButton>
                     </div>
                 </div>
             </div>
