22259d060db99d01373f524a956e50520d26e3ff
numangit <abdulnumanjob@gmail.com>
2022-11-12 20:48:51 +0600

    dynamic service cards added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -6,10 +6,10 @@ const Banner = () => {
             {/* bg image added using tailwind */}
             <div className="bg-[url('/src/assets/images/bg.png')] hero">
                 <div className="hero-content flex-col lg:flex-row-reverse p-8">
-                    <img src={chair} className="rounded-lg w-1/2 shadow-2xl" alt="" />
+                    <img src={chair} className="rounded-lg lg:w-1/2 shadow-2xl" alt="" />
                     <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+                        <p className="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
                         <button className="btn border-0	text-white bg-gradient-to-r from-primary to-secondary">Get Started</button>
                     </div>
                 </div>
