1f01c1d8cdf1e9f7bb78aa1ce4fbad5c58d62938
numangit <abdulnumanjob@gmail.com>
2022-11-12 18:37:14 +0600

    basic setups are made

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Routes/Routes/Routes';
 
 function App() {
   return (
-    <div>
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
+    <div className="max-w-[1440px] mx-auto">
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
