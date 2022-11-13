063179e89638f4662e35b65694ae17e6e13443bd
numangit <abdulnumanjob@gmail.com>
2022-11-13 13:06:35 +0600

    calendar date format and state added

--- src/Pages/Appointment/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment/Appointment.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppointmentBanner from '../AppointmentBanner/AppointmentBanner';
 
 const Appointment = () => {
+    const [selectedDate, setSelectedDate] = useState(new Date());
     return (
         <div>
-            <h1>This is appoint ment </h1>
-            <AppointmentBanner></AppointmentBanner>
+            <AppointmentBanner
+                selectedDate={selectedDate}
+                setSelectedDate={setSelectedDate}>
+            </AppointmentBanner>
         </div>
     );
 };
--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,10 +1,25 @@
+import { format } from 'date-fns';
 import React from 'react';
 import { DayPicker } from 'react-day-picker';
+import chair from "../../../assets/images/chair.png";
 
-const AppointmentBanner = () => {
+const AppointmentBanner = ({ selectedDate, setSelectedDate }) => {
     return (
         <div>
-            <DayPicker />;
+            {/* bg image added using tailwind */}
+            <div className="bg-[url('/src/assets/images/bg.png')] hero">
+                <div className="hero-content flex-col lg:flex-row-reverse p-10 lg:p-16  my-10 lg:my-20">
+                    <img src={chair} className="rounded-lg lg:w-1/2 shadow-2xl" alt="" />
+                    <div className='mx-auto'>
+                        <DayPicker
+                            mode='single'
+                            selected={selectedDate}
+                            onSelect={setSelectedDate}
+                        />
+                        <p className='text-center'>You picked {format(selectedDate, 'PP')}.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
