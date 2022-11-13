82889ac2023aae048f78b66d4523f7f965b7f9fb
numangit <abdulnumanjob@gmail.com>
2022-11-13 12:47:27 +0600

    react day picker added for calender functionality

--- src/Pages/Appointment/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment/Appointment.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import AppointmentBanner from '../AppointmentBanner/AppointmentBanner';
 
 const Appointment = () => {
     return (
         <div>
             <h1>This is appoint ment </h1>
+            <AppointmentBanner></AppointmentBanner>
         </div>
     );
 };
---
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { DayPicker } from 'react-day-picker';
+
+const AppointmentBanner = () => {
+    return (
+        <div>
+            <DayPicker />;
+        </div>
+    );
+};
+
+export default AppointmentBanner;
