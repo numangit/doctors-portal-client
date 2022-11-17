a5437ac53ae19a7245e4e6b7b2d5d5cc1e836779
numangit <abdulnumanjob@gmail.com>
2022-11-17 17:01:35 +0600

    MyAppointment page table added

--- src/Pages/Dashboard/MyAppointment/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointment/MyAppointment.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
 const MyAppointment = () => {
+
     return (
         <div>
+            <h3 className="text-3xl mb-5">My Appointments</h3>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Treatment</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
