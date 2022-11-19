8f97a4ebae366cfdd0de46ee76f5f890f1e7fdc7
numangit <abdulnumanjob@gmail.com>
2022-11-19 19:54:27 +0600

    image input is added and functionality to send the collected image input file data to post in img bb hosting service added

--- src/Pages/Dashboard/AddDoctor/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.js
@@ -1,15 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import toast from 'react-hot-toast';
+// import toast from 'react-hot-toast';
 // import { useNavigate } from 'react-router-dom';
 import Loading from '../../Shared/Loading/Loading';
 
 const AddDoctor = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
-
-    // const imageHostKey = process.env.REACT_APP_imgbb_key;
-
     // const navigate = useNavigate();
 
     //load the name of the appointment specialties from the api
@@ -22,44 +19,26 @@ const AddDoctor = () => {
         }
     })
 
-    // const handleAddDoctor = data => {
-    //     const image = data.image[0];
-    //     const formData = new FormData();
-    //     formData.append('image', image);
-    //     const url = `https://api.imgbb.com/1/upload?key=${imageHostKey}`
-    //     fetch(url, {
-    //         method: 'POST',
-    //         body: formData
-    //     })
-    //     .then(res => res.json())
-    //     .then(imgData => {
-    //         if(imgData.success){
-    //             console.log(imgData.data.url);
-    //             const doctor = {
-    //                 name: data.name, 
-    //                 email: data.email,
-    //                 specialty: data.specialty,
-    //                 image: imgData.data.url
-    //             }
+    //imagebb api key from .env file
+    const imageHostKey = process.env.REACT_APP_imgbb_key;
+    const handleAddDoctor = data => {
 
-    //             save doctor information to the database
-    //             fetch('http://localhost:5000/doctors', {
-    //                 method: 'POST',
-    //                 headers: {
-    //                     'content-type': 'application/json', 
-    //                     authorization: `bearer ${localStorage.getItem('accessToken')}`
-    //                 },
-    //                 body: JSON.stringify(doctor)
-    //             })
-    //             .then(res => res.json())
-    //             .then(result =>{
-    //                 console.log(result);
-    //                 toast.success(`${data.name} is added successfully`);
-    //                 navigate('/dashboard/managedoctors')
-    //             })
-    //         }
-    //     })
-    // }
+        //get image data from the form
+        const image = data.image[0];
+        const formData = new FormData();
+        formData.append('image', image);
+        //get imagebb url from the doc
+        const url = `https://api.imgbb.com/1/upload?key=${imageHostKey}`
+        //uploading the image in imgbb using post api
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(imgData => {
+                console.log(imgData);
+            })
+    }
 
     if (isLoading) {
         return <Loading></Loading>
@@ -68,7 +47,7 @@ const AddDoctor = () => {
     return (
         <div className='w-96 p-7'>
             <h2 className="text-4xl">Add A Doctor</h2>
-            <form>
+            <form onSubmit={handleSubmit(handleAddDoctor)}>
                 <div className="form-control w-full max-w-xs">
                     <label className="label"> <span className="label-text">Name</span></label>
                     <input type="text" {...register("name", {
@@ -94,15 +73,13 @@ const AddDoctor = () => {
                                 value={specialty.name}
                             >{specialty.name}</option>)
                         }
-
-
                     </select>
                 </div>
                 <div className="form-control w-full max-w-xs">
                     <label className="label"> <span className="label-text">Photo</span></label>
-                    <input type="file" {...register("image", {
-                        required: "Photo is Required"
-                    })} className="input input-bordered w-full max-w-xs" />
+                    <input type="file"
+                        {...register("image", { required: "Photo is Required" })}
+                        className="input input-bordered w-full max-w-xs" />
                     {errors.img && <p className='text-red-500'>{errors.img.message}</p>}
                 </div>
                 <input className='btn btn-accent w-full mt-4' value="Add Doctor" type="submit" />
