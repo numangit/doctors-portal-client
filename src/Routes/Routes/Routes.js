dc8a7352a60a00a55ec724276fc38bcee9a5c5b0
numangit <abdulnumanjob@gmail.com>
2022-11-19 20:34:48 +0600

    Manage doctors route and page added

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -4,6 +4,7 @@ import Main from "../../Layout/Main";
 import Appointment from "../../Pages/Appointment/Appointment/Appointment";
 import AddDoctor from "../../Pages/Dashboard/AddDoctor/AddDoctor";
 import Allusers from "../../Pages/Dashboard/Allusers/Allusers";
+import ManageDoctors from "../../Pages/Dashboard/ManageDoctors/ManageDoctors";
 import MyAppointment from "../../Pages/Dashboard/MyAppointment/MyAppointment";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
@@ -49,6 +50,10 @@ const router = createBrowserRouter([
             {
                 path: '/dashboard/adddoctor',
                 element: <AdminRoute><AddDoctor></AddDoctor></AdminRoute>
+            },
+            {
+                path: '/dashboard/managedoctors',
+                element: <AdminRoute><ManageDoctors></ManageDoctors></AdminRoute>
             }
         ]
     }
